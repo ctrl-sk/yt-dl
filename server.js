a8ab217a360da194d7215d96a9b512defed982a4
ctrl-sk <shashankkumar@ymail.com>
2025-12-01 18:07:24 +0530

    First commit

--- server.js
+++ server.js
@@ -58,6 +58,7 @@ app.get('/download', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
